1341420d591445c89358c1392ecb54d9898f280a
Sadik Bakiu <sbakiu@gmail.com>
2022-01-09 12:23:33 +0100

    Add logging

--- docker/app.py
+++ docker/app.py
@@ -1,8 +1,10 @@
 import pickle
 import numpy as np
+import logging
 
 from flask import Flask, request
 
+logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 # Uncomment when model file is available
 # model = pickle.load(open('model.pkl', 'rb'))
@@ -10,6 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    logging.info(f"Received request.")
     return "Hello"
 
 
@@ -18,6 +21,7 @@ def hello():
 @app.route('/predict', methods=['POST'])
 def predict():
     json = request.json
+    logging.info(f"Received json: {json}")
     # int_features = [float(x) for x in request.form.values()]
     # final_features = [np.array(int_features)]
     # prediction = model.predict(final_features)
